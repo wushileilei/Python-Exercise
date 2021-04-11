2e1e177b35aac09e942a9371006d62416af68efd
wushileilei <82299705+wushileilei@users.noreply.github.com>
2021-04-10 23:22:37 -0400

    Add files via upload

---
+++ lab11.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# Lab 11 by Shilei Wu
+
+def words():
+    word1= "Stapler before dawn"
+    word2= "Waiting in perfect silence"
+    word3= "Dreams of loose paper"
+    poem = word1 + "\n" + word2 + "\n" + word3
+    print poem
+    
+if __name__== '__main__':
+    words()
+
+
